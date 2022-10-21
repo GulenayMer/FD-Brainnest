7b9590038f4ce4fbae6d7b636d667fb99b7ea872
GulenayMer <mervegulenay@gmail.com>
2022-10-21 18:36:17 +0200

    ass2

--- project4/assignment2/rps_script.js
+++ project4/assignment2/rps_script.js
@@ -1,99 +1,80 @@
 
-/* ---  select the elements to manipulate --- */
-let options = document.querySelectorAll(".options");
+// --------------------------------------------//
+let selections = document.querySelectorAll(".selections");
 let score = document.getElementById("score");
 let result = document.getElementById("result");
 let restart = document.getElementById("restart");
-let over = document.getElementById("over");
-
-/* -----  computer choices randomly -----  */
-function getComputerChoice() 
-{ 
-   	let choices = ["rock", "paper", "scissors"]; 
-    let randomChoice = choices[Math.floor(Math.random() * choices.length)];
-	return randomChoice;
-};
 
 
-/*----  who is the winner? --- */
-function playRound(playerChoice, computerChoice)
-{
-
-	if (playerChoice === computerChoice)
-	{
-    	return "It is a Draw";
-	}
-	else if ((playerChoice === "rock" && computerChoice === "paper") 
-		|| (playerChoice === "paper" && computerChoice === "scissors")
-  		|| (playerChoice === "scissors" && computerChoice === "rock"))
-	{
-     	return "Computer wins this round"
-  	} 
-	else if ((playerChoice === "paper" && computerChoice === "rock") 
-			||(playerChoice === "scissors" && computerChoice === "paper")
-			|| (playerChoice === "rock" && computerChoice === "scissors"))
-	{
-      	return "Player wins this round"
-	}
-
-};
-
-let playerScore = 0;
-let computerScore = 0;
-/* ------------ show the winner -------- */
-function getScore(winner, playerScore, computerScore)
-{
- 	if (winner === "Player wins this round")
-	{
-    	playerScore++;
- 	}
- 	else if (winner === "Computer wins this round")
-	{
-    	computerScore++;
- 	}
-
-	score.innerHTML = `
- 		<h2 class="cDisplay">Computer: <span style= color:green>${computerScore}</span></h2>
- 		<h2 class="pDisplay">Player: <span style= color:green>${playerScore}</span></h2>`
-}
-
-function gameOver(playerScore, computerScore)
-{
-    if(playerScore === 2)
-       over.innerHTML += `\n\n GAME OVER... You lost...`;
-    else if(computerScore === 2)
-        over.innerHTML += `\n\n GAME OVER... You won!!`
-
-}
-/* ------ play ------------ */
-function play(e)
-{
+// ------------------------------------//
+// play
+function play(e){
     const playerChoice = e.target.id;
     const computerChoice = getComputerChoice();
-
-    const winner = playRound(playerChoice, computerChoice);
+    const winner = getWinner(playerChoice, computerChoice);
     result.innerHTML = winner;
-    getScore(winner, playerScore, computerScore);
-	gameOver(playerScore, computerScore);
+    getScore(winner);
 };
 
 
-/* ---------------- restart the game --------- */
-function restartGame()
-{
-    playerScore = 0;
-    computerScore = 0;
+// computer random choice
+ function getComputerChoice() { 
+    let selectionArr = ["rock", "paper", "scissors"]; 
+    let randomAnswer = selectionArr[Math.floor(Math.random() * selectionArr.length)];
+    return randomAnswer;
+    };
 
-    score.innerHTML = `
- 		<h2 class="cDisplay">Computer: <span style= color:#333>${computerScore }</span></h2>
- 		<h2 class="pDisplay">Player: <span style= color:#333>${playerScore }</span></h2>`
-		result.innerHTML = `<p>Play again!</p>`
+
+    // winner 
+function getWinner(p, c){
+  if(p === c){
+      return "It is a Draw";
+  } else if((p === "rock" && c === "paper") ||(p === "paper" && c === "scissors")
+  || (p === "scissors" && c === "rock")){
+     return "Computer wins"
+  } else if((p === "paper" && c === "rock") ||(p === "scissors" && c === "paper")
+  || (p === "rock" && c === "scissors")){
+      return "Player wins"
+  } else {
+      return "Nobody wins"
+  }
+};
+
+
+// show winner
+function getScore (winner){
+    let pScore = 0;
+    let cScore = 0;
+ if(winner === "Player wins"){
+    pScore++;
+ }
+ else if(winner === "Computer wins"){
+     cScore++;
+ }
+
+ score.innerHTML = `
+ <h2 class="cDisplay">Computer: <span style= color:green>${cScore}</span></h2>
+ <h2 class="pDisplay">Player: <span style= color:green>${pScore}</span></h2>
+ `
+ 
 }
 
-/* click the buttons , trigger the event  */
-options.forEach(choice => {
+// restart the game
+function restartGame(){
+    pScore = 0;
+    cScore = 0;
+    score.innerHTML = `
+ <h2 class="cDisplay">Computer: <span style= color:#333>${cScore}</span></h2>
+ <h2 class="pDisplay">Player: <span style= color:#333>${pScore}</span></h2>
+ `
+ result.innerHTML = `<p>Lets Play!</p>`
+}
+
+// click the buttons , trigger the event
+selections.forEach(choice => {
     choice.addEventListener("click", play);
 });
 
 restart.addEventListener("click", restartGame);
+
 
